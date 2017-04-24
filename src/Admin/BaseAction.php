5603fe83690b72e5c25e5924ac2143c9eca00ca6
popo1h <popo1h@sina.com>
2017-04-24 14:24:24 +0800

    Admin/BaseAction

---
+++ src/Admin/BaseAction.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AadminCore\Admin;
+
+use AadminCore\Core\Action;
+use AadminCore\Core\Response\Json;
+use AadminCore\Core\Response\View;
+
+abstract class BaseAction extends Action
+{
+    const STATUS_SUCCESS = 1;
+    const STATUS_ERROR = 0;
+
+    protected function buildViewPage($nowPage, $totalCount, $perPageCount, $pageParamName = 'page_id')
+    {
+        return [
+            'now_page' => $nowPage,
+            'total_count' => $totalCount,
+            'per_page_count' => $perPageCount,
+            'page_param_name' => $pageParamName,
+            'now_action_name' => static::getName(),
+        ];
+    }
+
+    protected function buildViewResponse($templateName, $templateData = [])
+    {
+        $response = new View();
+
+        $templateData['_now_action'] = static::getName();
+        $response->setTemplateName($templateName);
+        $response->setTemplateData($templateData);
+
+        return $response;
+    }
+
+    protected function buildAjaxJump($jumpActionName, $jumpCateName = null)
+    {
+        return [
+            'cate_name' => $jumpCateName,
+            'action_name' => $jumpActionName,
+        ];
+    }
+
+    protected function buildAjaxResponse($status, $info, $data = null, $jump = null)
+    {
+        $response = new Json();
+
+        $response->setData([
+            'status' => $status,
+            'info' => $info,
+            'data' => $data,
+            'jump' => $jump,
+        ]);
+
+        return $response;
+    }
+}
